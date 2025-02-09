7bdea6d90d6fd139822a6bfb55cedb37367fadc0
Mary Adewunmi <physayoemma@gmail.com>
2025-02-09 09:46:29 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/app/myphonebookapp/MyPhoneBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.myphonebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhoneBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhoneBookAppApplication.class, args);
+	}
+
+}
